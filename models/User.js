9a0a2e6322ddc1d8adc1479c02a14f67edf9b728
tfkjosh <jkg050604@gmail.com>
2023-12-19 21:21:19 -0600

    user model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+//set up mongoose
+const { Schema, model, Types } = require('mongoose');
+
+//set up user schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please use a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+// get friend count
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+//user model
+const User = model('User', userSchema);
+module.exports = User;
