151f2bdb55f1931b3839aebba8413a74347e4532
tfkjosh <jkg050604@gmail.com>
2023-12-28 16:13:32 -0600

    thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -85,4 +85,17 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with this ID!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
