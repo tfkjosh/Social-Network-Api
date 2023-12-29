14f9bed88ccbd581197b703b5d30bd38d65c208a
tfkjosh <jkg050604@gmail.com>
2023-12-29 13:04:46 -0600

    gitignore

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -71,31 +71,5 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
-  createReaction(req, res) {
-    Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $addToSet: { reactions: req.body } },
-      { runValidators: true, new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No thought found with this ID!" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-
-  deleteReaction(req, res) {
-    Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $pull: { reactions: { reactionId: req.params.reactionId } } },
-      { runValidators: true, new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No thought found with this ID!" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+ 
 };
